61b9dda388b102ec2ddae7b3fe6a41a474a54413
yaroslav2727 <90917825+yaroslav2727@users.noreply.github.com>
2023-11-12 09:16:09 +0000

    Deploying to gh-pages from @ yaroslav2727/goit-advancedjs-hw-01@18fb7f45428c542f28127faa1e4b65fe73602f46 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as o,l as a}from"./assets/vendor-78be7656.js";const e="videoplayer-current-time",t=new o("vimeo-player"),i=JSON.parse(localStorage.getItem(e))??0;t.setCurrentTime(i);t.on("timeupdate",a(l,1e3));function l({seconds:r}){localStorage.setItem(e,JSON.stringify(r))}
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as r,l as a}from"./assets/vendor-78be7656.js";const e="videoplayer-current-time",t=new r("vimeo-player"),l=localStorage.getItem(e)??0;t.setCurrentTime(l);t.on("timeupdate",a(m,1e3));function m({seconds:o}){localStorage.setItem(e,o)}
 //# sourceMappingURL=commonHelpers2.js.map
